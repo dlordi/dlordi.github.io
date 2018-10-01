4ab2123f486abe821d6732cdbc3a2e638bd8f49d
Domenico Lordi <domenico.lordi@gmail.com>
2018-10-01 23:31:30 +0200

    enrich ui

--- vuejs/app.js
+++ vuejs/app.js
@@ -1,6 +1,92 @@
+
+Vue.use(Vuetify);
+// Vue.use(VueRouter);
+// Vue.use(Vuex);
+
 const app = new Vue({
 	el: 'v-app',
 	data: {
-		drawer: true
-	}
+		navLeft: {
+			drawer: true,
+			tiles: [
+				{
+					title: 'Dashboard', icon: 'dashboard',
+					link: '/dashboard',
+				},
+				{
+					title: 'Settings', icon: 'settings',
+					link: '/settings',
+				},
+			],
+		},
+		tbrTop: {
+			items: [
+				{
+					title: 'Home', icon: 'dashboard',
+					link: '/home',
+				},
+				{
+					title: 'About', icon: 'question_answer',
+					link: '/about',
+				},
+			]
+		},
+		dlgSimple: {
+			visible: false,
+			title: '',
+			text: '',
+		},
+		dtbSimple: {
+			headers: [
+				{ value: 'h1', text: 'Header 1', align: 'left'},
+				{ value: 'h2', text: 'Header 2', align: 'center'},
+				{ value: 'h3', text: 'Header 2', align: 'right'},
+			],
+			items: [
+				{ h1: '1.1', h2: '1.2', h3: '1.3' },
+				{ h1: '2.1', h2: '2.2', h3: '2.3' },
+			],
+		},
+	},
+
+	methods: {
+		navLeftTileClick(tile) {
+			console.log(tile.link)
+		},
+		tbrTopItemClick(item) {
+			this.dlgSimple.title = 'Hey'
+			this.dlgSimple.text = item.link
+			this.dlgSimple.visible = true
+		},
+		dlgSimpleActionClick() {
+			this.dlgSimple.visible = false
+		},
+	},
+
+	/*
+	router: new VueRouter({
+		routes: [{
+			path: '/',
+			component: {template: '<h1>INDEX</h1>'},
+		}, {
+			path: '/home',
+			component: {template: '<h1>HOME</h1>'},
+		}, {
+			path: '/about',
+			component: {template: '<h1>ABOUT</h1>'},
+		}],
+		// non usa # per il routing ma si basa sull'errore 404
+		// che deve essere restituito dal web server
+		// mode: 'history',
+	}),
+	*/
+
+	/*
+	store: new Vuex.Store({
+		state: {},
+		mutations: {},
+		actions: {},
+		getters: {},
+	}),
+	*/
 })
